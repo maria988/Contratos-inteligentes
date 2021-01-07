d85c529e2611d65280ff34f08a6cb99894fe1d4b
maria988 <63420225+maria988@users.noreply.github.com>
2021-01-07 15:07:19 +0100

    Create test_descuento2.py

---
+++ stock_descuento/test/test_descuento2.py
@@ -0,0 +1,46 @@
+
+import pytest
+
+import brownie
+import time
+STOCK = 50
+PRECIO = 20
+INICIO = 3
+DURACION = 5
+PRECIODESCUENTO = 10
+@pytest.fixture
+def descuento2_contract(Descuento2, accounts):
+    yield Descuento2.deploy(STOCK,PRECIO,INICIO,DURACION,PRECIODESCUENTO,{'from': accounts[0]})
+
+def test_inicial(descuento2_contract,accounts):
+    assert descuento2_contract.vendedor() == accounts[0]
+    assert descuento2_contract.precio() == PRECIO
+    assert descuento2_contract.stock() == STOCK
+    assert descuento2_contract.pdescuento() == PRECIODESCUENTO
+
+def test_compra(descuento2_contract,accounts):
+    descuento2_contract.comprar({'from':accounts[1],'value':20})
+    assert descuento2_contract.stock() == 49 
+    time.sleep(5)
+    descuento2_contract.comprar({'from':accounts[2],'value':20})
+    assert descuento2_contract.stock() == 47
+    time.sleep(6)
+    descuento2_contract.comprar({'from':accounts[1],'value':20})
+    assert descuento2_contract.stock() == 46
+    descuento2_contract.terminar({'from':accounts[0]})
+    
+    
+def test_failed_transactions(descuento2_contract, accounts):
+    
+    with brownie.reverts("Precio adecuado"):
+        descuento2_contract.comprar({'from':accounts[1],'value':10})
+    
+    time.sleep(5)
+    descuento2_contract.comprar({'from':accounts[3],'value':250}) 
+    with brownie.reverts("Hay stock suficiente"):
+        descuento2_contract.comprar({'from':accounts[3],'value':500})
+    descuento2_contract.comprar({'from':accounts[3],'value':250})
+    with brownie.reverts("Hay stock"):
+        descuento2_contract.comprar({'from':accounts[3],'value':100})
+    with brownie.reverts("Vendedor"):
+        descuento2_contract.terminar({'from':accounts[1]})
