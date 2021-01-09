b81dd61fdf4d307cd3c81c1f809b86cec17e6330
maria988 <63420225+maria988@users.noreply.github.com>
2021-01-09 14:56:44 +0100

    Update test_apuestas3.py

--- Apuestas/test/test_apuestas3.py
+++ Apuestas/test/test_apuestas3.py
@@ -2,10 +2,10 @@
 import pytest
 
 import brownie
-
-DURACION = 90
-TIEMPO_INICIO = 10
-INITIAL_VALUE = 200
+import time
+DURACION = 3
+TIEMPO_INICIO = 5
+INITIAL_VALUE = 1
 
 @pytest.fixture
 def apuestas3_contract(Apuestas3, accounts):
@@ -18,6 +18,7 @@ def test_inicial(apuestas3_contract,accounts):
 
     
 def test_failed_transactions(apuestas3_contract, accounts):
+    apuestas3_contract.apostar(1,1,{'from': accounts[2],'value':4})
     
     with brownie.reverts("Jugador"):
         apuestas3_contract.apostar(1,0,{'from': accounts[0],'value':50})
@@ -25,3 +26,47 @@ def test_failed_transactions(apuestas3_contract, accounts):
     
     with brownie.reverts("Apuesta positiva"):
         apuestas3_contract.apostar(1,1,{'from': accounts[1],'value':0})
+        
+    with brownie.reverts("Despues de empezar"):
+        apuestas3_contract.mitad({'from': accounts[1],'value':0})
+        
+    with brownie.reverts("Casa"):
+        apuestas3_contract.necesario({'from': accounts[1]})    
+    
+    with brownie.reverts("Despues de empezar"):
+        apuestas3_contract.necesario({'from': accounts[0]})
+        
+    time.sleep(5)
+    with brownie.reverts("Antes de empezar"):
+        apuestas3_contract.apostar(1,1,{'from': accounts[1],'value':0})
+        
+    with brownie.reverts("Casa"):
+        apuestas3_contract.mitad({'from': accounts[1],'value':0})
+    
+    with brownie.reverts("Valor suficiente"):
+        apuestas3_contract.mitad({'from': accounts[0],'value':0})
+    
+    with brownie.reverts("Despues de terminar"):
+        apuestas3_contract.devolver({'from': accounts[0]})
+        
+    with brownie.reverts("Casa"):
+        apuestas3_contract.ganadores(1,1,{'from': accounts[1]})  
+    
+    with brownie.reverts("Despues de terminar"):
+        apuestas3_contract.ganadores(1,1,{'from': accounts[0]}) 
+    
+    with brownie.reverts("Despues de terminar"):
+        apuestas3_contract.ganar((accounts[1],1,1,20),{'from':accounts[1]}) 
+        
+    time.sleep(4)
+    
+    with brownie.reverts("Casa"):
+        apuestas3_contract.devolver({'from': accounts[1]})
+        
+    with brownie.reverts("Ha invertido"):
+        apuestas3_contract.devolver({'from': accounts[0]})
+    
+    apuestas3_contract.mitad({'from': accounts[0],'value':100})
+    
+    with brownie.reverts("Se han devuelto a todos"):
+        apuestas3_contract.finalizacion({'from': accounts[0]})
