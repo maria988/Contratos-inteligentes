b067cbe67c39067f8630e0b38de261a242305835
maria988 <63420225+maria988@users.noreply.github.com>
2021-01-18 20:00:46 +0100

    Create test_telefono.py

---
+++ Telefono/test/test_telefono.py
@@ -0,0 +1,76 @@
+
+import pytest
+
+import brownie
+
+import time
+
+PRECIOTIEMPO = 1
+ESTABLLAMADA = 3
+TELEFONO = "925459402"
+CANTIDAD = 3
+TELEFONO2 = "913930183"
+@pytest.fixture
+def telefono_contract(telefono, accounts):
+    yield telefono.deploy(PRECIOTIEMPO,ESTABLLAMADA,TELEFONO,{'from': accounts[0]})
+
+def test_inicial(telefono_contract,accounts):
+    assert telefono_contract.empresa() == accounts[0]
+    assert telefono_contract.precioTiempo() == PRECIOTIEMPO
+    assert telefono_contract.estabLlamada() == ESTABLLAMADA
+    assert telefono_contract.telefono() == TELEFONO
+    
+    
+def test_failed_transactions(telefono_contract, accounts):
+    
+    with brownie.reverts("Empresa"):
+        telefono_contract.recargar(TELEFONO, accounts[3],{'from':accounts[1],'value':CANTIDAD})
+    
+    with brownie.reverts("Telefono correcto"):
+        telefono_contract.recargar(TELEFONO2, accounts[0],{'from':accounts[1],'value':CANTIDAD})
+   
+    with brownie.reverts("Valor positivo"):
+        telefono_contract.recargar(TELEFONO, accounts[0],{'from':accounts[1],'value':0})
+         
+    telefono_contract.recargar(TELEFONO, accounts[0],{'from':accounts[1],'value':CANTIDAD})
+    
+    with brownie.reverts("Telefono correcto"):
+        telefono_contract.saldo(TELEFONO2,{'from':accounts[1]})
+        
+    assert telefono_contract.saldo(TELEFONO,{'from':accounts[1]}) == CANTIDAD
+    
+    with brownie.reverts("Telefono correcto"):
+        telefono_contract.colgar(TELEFONO2,{'from':accounts[0]})
+     
+    with brownie.reverts("Llamando"):
+        telefono_contract.colgar(TELEFONO,{'from':accounts[0]})
+        
+    with brownie.reverts("Telefono correcto"):
+        telefono_contract.llamar(TELEFONO2,{'from':accounts[0]})
+        
+    with brownie.reverts("Suficiente"):
+        telefono_contract.llamar(TELEFONO,{'from':accounts[0]})
+    
+    with brownie.reverts("Telefono correcto"):
+        telefono_contract.cortar(TELEFONO2,{'from':accounts[0]})
+    
+    with brownie.reverts("Empresa"):
+        telefono_contract.cortar(TELEFONO,{'from':accounts[2]})
+        
+    with brownie.reverts("Llamando"):
+        telefono_contract.cortar(TELEFONO,{'from':accounts[0]})
+    
+    telefono_contract.recargar(TELEFONO, accounts[0],{'from':accounts[1],'value':CANTIDAD})
+    telefono_contract.llamar(TELEFONO,{'from':accounts[0]})
+    
+    with brownie.reverts("Superado"):
+        telefono_contract.cortar(TELEFONO,{'from':accounts[0]})
+        
+    time.sleep(5)
+    
+    telefono_contract.cortar(TELEFONO,{'from':accounts[0]})
+    
+    assert telefono_contract.saldo(TELEFONO,{'from':accounts[1]}) == 0
+    
+    
+    
