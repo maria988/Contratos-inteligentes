66a149c7329f5840db28eb9cf72332e67d04ded1
maria988 <63420225+maria988@users.noreply.github.com>
2021-03-02 23:05:24 +0100

    Update test_bingo2.py

--- Bingo/test/test_bingo2.py
+++ Bingo/test/test_bingo2.py
@@ -2,7 +2,8 @@
 import pytest
 
 import brownie
-import time
+
+import math
 
 PRECIO = 2
 PORC_LINEA = 10
@@ -19,6 +20,38 @@ def test_inicial(bingo2_contract,accounts):
     assert bingo2_contract.porc_linea() == PORC_LINEA
     assert bingo2_contract.porc_bingo() == PORC_BINGO
     assert bingo2_contract.acumulado() == INITIAL_VALUE   
+    
+def test_uso(bingo2_contract,accounts):
+    bingo2_contract.comprarcarton({'from':accounts[1],'value':PRECIO})
+    bingo2_contract.comprarcarton({'from':accounts[2],'value':2*PRECIO})
+    assert bingo2_contract.acumulado() == PRECIO *3 + INITIAL_VALUE
+    assert bingo2_contract.ganarconbingo({'from':accounts[1]}) == ((bingo2_contract.acumulado()* PORC_BINGO)/100)
+    assert bingo2_contract.ganarconlinea({'from':accounts[1]}) == ((bingo2_contract.acumulado() * PORC_LINEA)/100)
+    
+    bingo2_contract.empezar({'from':accounts[0]})
+    assert bingo2_contract.empezado()
+    bingo2_contract.ponernumero(2,{'from':accounts[0]})
+    bingo2_contract.ponernumero(60,{'from':accounts[0]})
+    bingo2_contract.ponernumero(71,{'from':accounts[0]})
+    bingo2_contract.ponernumero(30,{'from':accounts[0]})
+    bingo2_contract.ponernumero(4,{'from':accounts[0]})
+    
+    bingo2_contract.linea(2,60,71,30,4,{'from':accounts[1]})
+    assert bingo2_contract.yalinea()
+    assert bingo2_contract.balance() == math.ceil(bingo2_contract.acumulado() - ((bingo2_contract.acumulado() * PORC_LINEA)/100))
+    
+    bingo2_contract.ponernumero(6,{'from':accounts[0]})
+    bingo2_contract.ponernumero(78,{'from':accounts[0]})
+    bingo2_contract.ponernumero(22,{'from':accounts[0]})
+    bingo2_contract.ponernumero(1,{'from':accounts[0]})
+    bingo2_contract.ponernumero(79,{'from':accounts[0]})
+    bingo2_contract.ponernumero(50,{'from':accounts[0]})
+    bingo2_contract.ponernumero(59,{'from':accounts[0]})
+    bingo2_contract.ponernumero(89,{'from':accounts[0]})
+    bingo2_contract.ponernumero(5,{'from':accounts[0]})
+    bingo2_contract.ponernumero(33,{'from':accounts[0]})
+    
+    bingo2_contract.bingo(2,60,71,30,4,6,78,22,1,79,50,59,89,5,33,{'from':accounts[2]})
 
 def test_failed_transactions(bingo2_contract, accounts):
     bingo2_contract.comprarcarton({'from': accounts[3],'value':4})
@@ -112,5 +145,7 @@ def test_failed_transactions(bingo2_contract, accounts):
     
     
     bingo2_contract.bingo(2,4,21,56,89,12,65,8,54,3,45,25,86,76,11,{'from': accounts[1]})
+    
+    
     
     
